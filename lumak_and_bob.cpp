0d8ead1ae9cba8eee1dd3296e9147eb803593ec9
edwinmwiti <emwiti658@gmail.com>
2020-12-16 15:00:25 +0300

    lumak and bob weight problem

---
+++ lumak_and_bob.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a, b, n_yrs;
+
+  n_yrs = 0;
+  cin>>a>>b;
+  
+  if(a > 10 || b > 10){
+    return 0;
+  }else{
+    do{
+      /*
+	while lumak's weight is less than bob's, triple lumak's, double bob's
+       */
+      a *= 3;
+      b *=2;
+      n_yrs++;
+    }while(a <= b);
+  }
+  
+  cout<<n_yrs<<endl;
+}
+  
